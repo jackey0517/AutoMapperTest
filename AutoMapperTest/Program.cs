d988d27d472b477db8a2a7fa87eba53525373a3e
Gavin Fang <fangshanzhang@live.com>
2016-03-06 10:57:19 +0800

    Initialization

---
+++ AutoMapperTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperTest
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			var tests = new Tests();
+
+			try
+			{
+				tests.SmokingTest();
+			}
+			catch ( AutoMapperConfigurationException ex )
+			{
+				Console.WriteLine( ex );
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
